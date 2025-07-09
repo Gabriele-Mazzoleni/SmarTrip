aa7062e1c2d6802de45e3cb3f1d976503fa2acb7
DanieleGotti <danielegotti02@gmail.com>
2025-07-09 23:41:18 +0200

    Doc test e metriche

---
+++ code/java/database/codemr/database/html/html_report_database/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"database working set: {database}", "Number of Highly Problematic Classes":0, "Number of Entities":20, "Number of Problematic Classes":0, "Class Lines of Code":1108, "Number of External Packages":8, "Number of Packages":5, "Number of External Entities":65}
